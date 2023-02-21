f13e9a04f870c1ecd04ceccd26aff7f74e505f1a
Gregg Trimarchi <gttrimarchig@gmail.com>
2023-02-21 00:53:49 -0700

    actual initial commit

---
+++ RexlyiOS/RexlyiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RexlyiOSApp.swift
+//  RexlyiOS
+//
+//  Created by Gregg Trimarchi on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct RexlyiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
